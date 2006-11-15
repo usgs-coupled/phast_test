84f4a4d9d570e643a35c02e2dfc472d64aaae41e
Scott R Charlton <charlton@usgs.gov>
2006-11-15 04:57:45 +0000

    moved unit/units into srcinput
    git-svn-id: svn://136.177.114.72/svn_GW/phastpp/trunk@1479 1feff8c3-07ed-0310-ac33-dd36852eb9cd

---
+++ srcinput/unit_impl.cxx
@@ -0,0 +1,144 @@
+#include "StdAfx.h"
+
+#define EXTERNAL extern
+#include "srcinput/hstinpt.h"
+#undef EXTERNAL
+
+#include <cassert>
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+cunit::cunit(void)
+: input(0)
+, si(0)
+, input_to_si(1.0)
+, input_to_user(1.0)
+, defined(FALSE)
+{
+}
+
+cunit::cunit(const char* si)
+: input(0)
+, si(0)
+, input_to_si(1.0)
+, input_to_user(1.0)
+, defined(FALSE)
+{
+	this->si = new char[::strlen(si) + 1];
+	::strcpy(this->si, si);
+}
+
+cunit::~cunit(void)
+{
+	delete si;
+	delete input;
+}
+
+cunit::cunit(const cunit& src)
+: input(0)
+, si(0)
+, input_to_si(src.input_to_si)
+, input_to_user(src.input_to_user)
+, defined(src.defined)
+{
+	if (src.input) {
+		this->input = new char[::strlen(src.input) + 1];
+		::strcpy(this->input, src.input);
+	}
+	if (src.si) {
+		this->si = new char[::strlen(src.si) + 1];
+		::strcpy(this->si, src.si);
+	}
+}
+
+cunit& cunit::operator=(const cunit& rhs)
+{
+	if (this != &rhs) {
+		delete this->input;
+		this->input = 0;
+		if (rhs.input) {
+			this->input = new char[::strlen(rhs.input) + 1];
+			::strcpy(this->input, rhs.input);
+		}
+
+		delete this->si;
+		this->si = 0;
+		if (rhs.si) {
+			this->si = new char[::strlen(rhs.si) + 1];
+			::strcpy(this->si, rhs.si);
+		}
+
+		this->input_to_si   = rhs.input_to_si;
+		this->input_to_user = rhs.input_to_user;
+		this->defined       = rhs.defined;
+	}
+	return *this;
+}
+
+int cunit::set_input(const char* input)
+{
+	if (!input) return ERROR;
+
+	if (this->defined == TRUE) {
+		assert(this->input);
+		delete this->input;
+	}
+	this->defined = TRUE;
+	this->input = new char[::strlen(input) + 1];
+	::strcpy(this->input, input);
+
+	assert(this->si);
+	int n = ::units_conversion(this->input, this->si, &this->input_to_si, FALSE);
+	if (n != OK) {
+		delete this->input;
+		this->input = 0;
+		this->defined = FALSE;
+	}
+	else {
+		if (this->input_to_si == 1.0) {
+			delete this->input;
+			this->input = 0;
+			this->defined = FALSE;
+		}
+	}
+	return n;
+}
+
+const char* cunit::c_str(void)const
+{
+	if (this->defined == TRUE) {
+		assert(this->input);
+		return this->input;
+	}
+	else {
+		assert(this->si);
+		return this->si;
+	}
+}
+
+void cunit::define(const char* input)
+{
+	assert(input);
+	if (!input) return;
+	if (this->defined == TRUE)
+	{
+		assert(this->input);
+		delete this->input;
+	}
+	this->defined = TRUE;
+	this->input = new char[::strlen(input) + 1];
+	::strcpy(this->input, input);
+}
+
+void cunit::undefine(void)
+{
+	if (this->defined == TRUE)
+	{
+		assert(this->input);
+	}
+	delete this->input;
+	this->input = 0;
+	this->defined = FALSE;
+}
---
+++ srcinput/units_impl.cxx
@@ -0,0 +1,85 @@
+#include "StdAfx.h"
+
+#include <cassert>
+
+#define EXTERNAL extern
+#include "srcinput/hstinpt.h"
+#undef EXTERNAL
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+cunits::cunits(void)
+: time("s")
+, horizontal("m")
+, vertical("m")
+, head("m")
+, k("m/s")	
+, s("1/m")	
+, alpha("m")	
+, leaky_k("m/s")	
+, leaky_thick("m")	
+, flux("m/s")	
+, well_diameter("m")	
+, well_pumpage("m^3/s")	
+, river_bed_k("m/s")	
+, river_bed_thickness("m")	
+{
+}
+
+cunits::~cunits(void)
+{
+}
+
+struct cunits& cunits::operator=(const struct cunits& rhs)
+{
+	assert(::strcmp(this->horizontal.si,           rhs.horizontal.si)          == 0);
+	assert(::strcmp(this->vertical.si,             rhs.vertical.si)            == 0);
+	assert(::strcmp(this->head.si,                 rhs.head.si)                == 0);
+	assert(::strcmp(this->k.si,                    rhs.k.si)                   == 0);
+	assert(::strcmp(this->s.si,                    rhs.s.si)                   == 0);
+	assert(::strcmp(this->alpha.si,                rhs.alpha.si)               == 0);
+	assert(::strcmp(this->leaky_k.si,              rhs.leaky_k.si)             == 0);
+	assert(::strcmp(this->leaky_thick.si,          rhs.leaky_thick.si)         == 0);
+	assert(::strcmp(this->flux.si,                 rhs.flux.si)                == 0);
+	assert(::strcmp(this->well_diameter.si,        rhs.well_diameter.si)       == 0);
+	assert(::strcmp(this->well_pumpage.si,         rhs.well_pumpage.si)        == 0);
+	assert(::strcmp(this->river_bed_k.si,          rhs.river_bed_k.si)         == 0);
+	assert(::strcmp(this->river_bed_thickness.si,  rhs.river_bed_thickness.si) == 0);
+	if (this != &rhs) {
+		this->time                = rhs.time;
+		this->horizontal          = rhs.horizontal;
+		this->vertical            = rhs.vertical;
+		this->head                = rhs.head;
+		this->k                   = rhs.k;
+		this->s                   = rhs.s;
+		this->alpha               = rhs.alpha;
+		this->leaky_k             = rhs.leaky_k;
+		this->leaky_thick         = rhs.leaky_thick;
+		this->flux                = rhs.flux;
+		this->well_diameter       = rhs.well_diameter;
+		this->well_pumpage        = rhs.well_pumpage;
+		this->river_bed_k         = rhs.river_bed_k;
+		this->river_bed_thickness = rhs.river_bed_thickness;
+	}
+	return *this;
+}
+
+void cunits::undefine(void)
+{
+	this->time.undefine();
+	this->horizontal.undefine();
+	this->vertical.undefine();
+	this->head.undefine();
+	this->k.undefine();	
+	this->s.undefine();	
+	this->alpha.undefine();	
+	this->leaky_k.undefine();	
+	this->leaky_thick.undefine();	
+	this->flux.undefine();	
+	this->well_diameter.undefine();	
+	this->well_pumpage.undefine();	
+	this->river_bed_k.undefine();	
+	this->river_bed_thickness.undefine();	
+}
