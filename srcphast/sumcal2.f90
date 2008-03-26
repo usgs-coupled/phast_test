03c409d32a78877a84e1a677c54f90de79759ad0
David L Parkhurst <dlpark@usgs.gov>
2008-03-26 22:55:00 +0000

    "or" test resulted in a array over run. Split test.
    git-svn-id: svn://136.177.114.72/svn_GW/phastpp/trunk@2781 1feff8c3-07ed-0310-ac33-dd36852eb9cd

--- srcphast/sumcal2.f90
+++ srcphast/sumcal2.f90
@@ -311,7 +311,10 @@ SUBROUTINE sumcal2
            ! ... Calculate fraction of cell that is newly saturated using
            ! ...      residual volume of fluid from cell below
            frac(m+nxy)=(frac(m)-1._kdp)*pv(m)/pv(m+nxy)
-           IF(k+1 == nz .OR. ibc(m+2*nxy) == -1) THEN
+           !IF(k+1 == nz .OR. ibc(m+2*nxy) == -1) THEN
+           IF(k+1 == nz) THEN
+              zfs(mt)=.5_kdp*(z1+z0+frac(m+nxy)*(z1-z0))     ! half cell thickness
+           ELSE IF (ibc(m+2*nxy) == -1) then
               zfs(mt)=.5_kdp*(z1+z0+frac(m+nxy)*(z1-z0))     ! half cell thickness
            ELSE
               z2=z(k+2)
