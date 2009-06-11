903134d52dbd21add589d910a92d0ef08b1d9b23
David L Parkhurst <dlpark@usgs.gov>
2009-06-11 22:51:39 +0000

    Cleaned up property.h enum
    git-svn-id: svn://136.177.114.72/svn_GW/phastpp/trunk@3527 1feff8c3-07ed-0310-ac33-dd36852eb9cd

--- property.h
+++ property.h
@@ -12,9 +12,9 @@ enum PROP_TYPE
 	PROP_POINTS = 105,
 	PROP_XYZ = 106,
 	//PROP_MIX_POINTS = 107,
-	PROP_MIX_XYZ = 108,
+	//PROP_MIX_XYZ = 108,
 	//PROP_MIX_CONSTANT = 109,
-	PROP_XYZT = 110,
+	PROP_XYZT = 110
 	//PROP_MIX_XYZT = 111
 };
 /* ----------------------------------------------------------------------
