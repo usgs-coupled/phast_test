1914abc372bfb36149ebb9ce9de8eb0577a86c0a
Scott R Charlton <charlton@usgs.gov>
2010-06-21 23:36:00 +0000

    should be able to build phast 2.0
    git-svn-id: svn://136.177.114.72/svn_GW/phastpp/trunk@4567 1feff8c3-07ed-0310-ac33-dd36852eb9cd

--- packages/dist_win/build.sh
+++ packages/dist_win/build.sh
@@ -3,11 +3,10 @@
 # Requirements:
 #
 # o Visual Studio 2005
-# o Intel(R) Fortran Compiler 9.1
-# o Visual Studio 6 w/ SP5
-# o Visual Fortran 6.1a
-# o InstallShield Professional 6.31
-# o ModelViewer 1.0
+# o Intel(R) Fortran Compiler >= 9.1
+# o Visual Studio 6 w/ (SP >= SP5)
+# o Visual Fortran 6.1.A
+# o ModelViewer 1.3
 # o jdk >= 1.4.1
 # o hdf5 >= 1.6.5
 # o htmlhelp
@@ -25,7 +24,6 @@
 #
 #
 # build using:
-# ./phast-1.0-1.sh cvsexport
 # time ./phast-1.0-1.sh all 2>&1 | tee phast-1.0-1.sh.build.log
 #
 ##################################################################
@@ -71,7 +69,6 @@ export src_pkg_name=${FULLPKG}-src.tar.bz2
 export src_patch_name=${FULLPKG}.patch
 export bin_pkg_name=${FULLPKG}.tar.bz2
 export src_orig_pkg=${topdir}/${src_orig_pkg_name}
-export src_orig_pkg_mv=${topdir}/Mv-${VER}.tar.gz
 export src_pkg=${topdir}/${src_pkg_name}
 export src_patch=${topdir}/${src_patch_name}
 export bin_pkg=${topdir}/${bin_pkg_name}
@@ -103,30 +100,6 @@ PHAST_SLN=`cygpath -w ./src/phast/win32_2005/phastpp.sln`
 PHASTINPUT_SLN=`cygpath -w ./src/phastinput/vc80/phastinput.sln`
 PHASTHDF_SLN=`cygpath -w ./src/phasthdf/win32/phastexport.sln`
 MSI_SLN=`cygpath -w ./msi/msi.sln`
-
-
-# InstallShield settings (based on exported build file
-# IS_COMPILER=`locate Compile.exe | grep InstallShield`
-# IS_BUILDER="`locate ISBuild.exe | grep InstallShield`"
-IS_COMPILER="/cygdrive/c/Program Files/Common Files/InstallShield/IScript/Compile.exe"
-IS_BUILDER="/cygdrive/c/Program Files/InstallShield/Professional - Standard Edition/Program/ISBuild.exe"
-
-IS_INSTALLPROJECT=`cygpath -w "${objdir}/packages/win32-is/phast.ipr"`
-IS_CURRENTBUILD=SingleDisk
-
-IS_HOME=`echo "${IS_BUILDER}" | sed -e 's^/Program/ISBuild.exe$^^'`
-IS_HOME=`cygpath -w "${IS_HOME}"`
-
-IS_INCLUDEIFX=${IS_HOME}\\Script\\IFX\\Include
-IS_INCLUDEISRT=${IS_HOME}\\Script\\ISRT\\Include
-IS_INCLUDESCRIPT=`cygpath -w "${objdir}/packages/win32-is/Script Files"`
-IS_LINKPATH1="-LibPath${IS_HOME}\\Script\\IFX\\Lib"
-IS_LINKPATH2="-LibPath${IS_HOME}\\Script\\ISRT\\Lib"
-IS_RULFILES=`cygpath -w "${objdir}/packages/win32-is/Script Files/Setup.rul"`
-IS_LIBRARIES="isrt.obl ifx.obl"
-IS_DEFINITIONS=""
-IS_SWITCHES="-w50 -e50 -v3 -g"
-export PHASTTOPDIR="`cygpath -w "${instdir}${prefix}"`"
 
 # Modelviewer 
 export MODELVIEWER_1_3="/cygdrive/c/Program Files/USGS/Model Viewer 1.3/"
@@ -251,38 +224,10 @@ precheck() {
   fi )
 }
 
-# Note: cp -al and ln aren't working if no ownership of source
-# after upgrading xp/cygwin 12/15/2004
-#
-svnexport() {
-  (precheck && \
-  cd ${topdir} && \
-  svn export -r ${REL} http://internalbrr.cr.usgs.gov/svn_GW/ModelViewer/trunk ModelViewer && \
-  cd ${topdir} && \
-# external files
-  mkdir -p Redist && \
-  cp "`cygpath "${MODELVIEWER_1_3}"`/notice.txt"            ${topdir}/Redist/. && \
-  cp "`cygpath "${MODELVIEWER_1_3}"`/readme.txt"            ${topdir}/Redist/. && \
-  cp "`cygpath "${MODELVIEWER_1_3}"`/doc/ofr02-106.pdf"     ${topdir}/Redist/. && \
-  cp "`cygpath "${MODELVIEWER_1_3}"`/bin/DFORRT.DLL"        ${topdir}/Redist/. && \
-  cp "`cygpath "${MODELVIEWER_1_3}"`/bin/hdf5dll.dll"       ${topdir}/Redist/. && \
-  cp "`cygpath "${MODELVIEWER_1_3}"`/bin/lf90.eer"          ${topdir}/Redist/. && \
-  cp "`cygpath "${MODELVIEWER_1_3}"`/bin/lf90wiod.dll"      ${topdir}/Redist/. && \
-  cp "`cygpath "${MODELVIEWER_1_3}"`/bin/modview.chm"       ${topdir}/Redist/. && \
-  cp "`cygpath "${MODELVIEWER_1_3}"`/bin/szlibdll.dll"      ${topdir}/Redist/. && \
-  cp "`cygpath "${MODELVIEWER_1_3}"`/bin/vtkCommon.dll"     ${topdir}/Redist/. && \
-  cp "`cygpath "${MODELVIEWER_1_3}"`/bin/vtkFiltering.dll"  ${topdir}/Redist/. && \
-  cp "`cygpath "${MODELVIEWER_1_3}"`/bin/vtkGraphics.dll"   ${topdir}/Redist/. && \
-  cp "`cygpath "${MODELVIEWER_1_3}"`/bin/vtkImaging.dll"    ${topdir}/Redist/. && \
-  cp "`cygpath "${MODELVIEWER_1_3}"`/bin/vtkRendering.dll"  ${topdir}/Redist/. && \
-  cp "`cygpath "${MODELVIEWER_1_3}"`/bin/zlib1.dll"         ${topdir}/Redist/. )
-}
-
 prep() {
   (cd ${topdir} && \
   tar xvzf ${src_orig_pkg} && \
   cd ${topdir}/${FULLPKG} && \
-##  tar xvzf ${src_orig_pkg_mv} && \
   cd ${topdir} && \
   if [ -f ${src_patch} ] ; then \
     patch -p0 --binary < ${src_patch} ;\
@@ -294,27 +239,7 @@ conf() {
   (cd ${objdir} && \
   rm -rf * && \
 # copy links to ${objdir} for building
-  find ${srcdir} -mindepth 1 -maxdepth 1 ! -name .build ! -name .inst ! -name .sinst -exec cp -al {} . \; && \
-# create missing InstallShield directories if nec
-  if [ ! -d "${objdir}/packages/win32-is/Setup Files/Compressed Files/Language Independent/Intel 32" ] ; then \
-    mkdir -p "${objdir}/packages/win32-is/Setup Files/Compressed Files/Language Independent/Intel 32" ; \
-  fi && \
-  if [ ! -d "${objdir}/packages/win32-is/Setup Files/Compressed Files/0009-English/OS Independent" ] ; then \
-    mkdir -p "${objdir}/packages/win32-is/Setup Files/Compressed Files/0009-English/OS Independent" ; \
-  fi && \
-  if [ ! -d "${objdir}/packages/win32-is/Setup Files/Compressed Files/0009-English/Intel 32" ] ; then \
-    mkdir -p "${objdir}/packages/win32-is/Setup Files/Compressed Files/0009-English/Intel 32" ; \
-  fi && \
-# make sure phast.ipr exists and has a size greater than zero
-  if [ ! -s ${srcdir}/packages/win32-is/phast.ipr ] ; then \
-    echo "Error: MISSING: ${srcdir}/packages/win32-is/phast.ipr"; \
-    exit 1; \
-  fi && \
-# make sure value.shl exists and has a size greater than zero
-  if [ ! -s ${srcdir}/packages/win32-is/String\ Tables/0009-English/value.shl ] ; then \
-    echo "Error: MISSING: ${srcdir}/packages/win32-is/String\ Tables/0009-English/value.shl"; \
-    exit 1; \
-  fi )
+  find ${srcdir} -mindepth 1 -maxdepth 1 ! -name .build ! -name .inst ! -name .sinst -exec cp -al {} . \; )
 }
 
 build() {
@@ -332,7 +257,6 @@ build() {
 # build model viewer
   "${MSDEV}" `cygpath -w ./ModelViewer/MvProject.dsw` /MAKE "ModelViewer - Win32 Release" /REBUILD && \
 # build phast.msi
-#  "${DEVENV}" "${MSI_SLN}" /out msi.log /build Release )
   MSBuild.exe "${MSI_SLN}" /p:Configuration=Release /p:TargetName=${FULLPKG} /p:Major=${MAJOR} /p:Minor=${MINOR} /p:Build=${REL} )
 }
 
@@ -345,161 +269,45 @@ check() {
 
 clean() {
   (cd ${objdir} && \
-###  msdev `cygpath -w ./phastexport/phastexport.dsw` /MAKE "phastexport - Win32 Release" /CLEAN && \
-###  msdev `cygpath -w ./srcinput/win32/phastinput.dsw` /MAKE "phastinput - Win32 Release" /CLEAN && \
-  ant -buildfile export/build.xml clean )
-# clean phast
-###  msdev `cygpath -w ./srcphast/win32/phast.dsw` /MAKE "phast - Win32 merge" /CLEAN && \
-###  msdev `cygpath -w ./srcphast/win32/phast.dsw` /MAKE "phast - Win32 ser" /CLEAN && \
-# clean ModelViewer
-###  msdev `cygpath -w ./Mv/MvProject.dsw` /MAKE "ModelViewer - Win32 Release" /CLEAN )
+# clean phasthdf.exe
+  "${DEVENV}" "${PHASTHDF_SLN}" /Clean Release && \
+# clean phastinput.exe
+  "${DEVENV}" "${PHASTINPUT_SLN}" /Clean Release && \
+# clean phast.jar
+  "${ANT}" -buildfile ./src/phasthdf/build.xml clean && \
+# clean merge/phast.exe
+  "${DEVENV}" "${PHAST_SLN}" /Clean merge && \
+# clean ser/phast.exe
+  "${DEVENV}" "${PHAST_SLN}" /Clean ser && \
+# clean model viewer
+  "${MSDEV}" `cygpath -w ./ModelViewer/MvProject.dsw` /MAKE "ModelViewer - Win32 Release" /CLEAN && \
+# clean phast.msi
+  MSBuild.exe "${MSI_SLN}" /t:Clean /p:Configuration=Release /p:TargetName=${FULLPKG} /p:Major=${MAJOR} /p:Minor=${MINOR} /p:Build=${REL} && \
+  rm -rf msi/bin && \
+  rm -rf msi/obj )
 }
 
 install() {
-  (cd ${instdir} && \
-# create directory structure
-  /usr/bin/mkdir -p ${instdir}/logs && \
-  /usr/bin/mkdir -p ${instdir}${prefix}/bin && \
-  /usr/bin/mkdir -p ${instdir}${prefix}/doc && \
-  /usr/bin/mkdir -p ${instdir}${prefix}/database && \
-  /usr/bin/mkdir -p ${instdir}${prefix}/examples && \
-# create examples directories
-  for arg in $EXAMPLES; do
-    /usr/bin/mkdir -p ${instdir}${prefix}/examples/${arg}
-  done && \
-  /usr/bin/mkdir -p ${instdir}${prefix}/mv && \
-  /usr/bin/mkdir -p ${instdir}${prefix}/mv/doc && \
-  /usr/bin/mkdir -p ${instdir}${prefix}/mv/bin && \
-  /usr/bin/mkdir -p ${instdir}${prefix}/export/bin && \
-  /usr/bin/mkdir -p ${instdir}${prefix}/export/lib && \
-  /usr/bin/mkdir -p ${instdir}${prefix}/export/lib/Win32 && \
-  /usr/bin/install -m 644 ${objdir}/doc/README \
-    ${instdir}${prefix}/README.txt && \
-# (bin dir) phast-ser, phast-mpich2, hdf5dll.dll, zlib1.dll, szlibdll.dll phast.bat
-  /usr/bin/install -m 755 ${objdir}/src/phastinput/vc80/Release/phastinput.exe \
-    ${instdir}${prefix}/bin/phastinput.exe && \
-  /usr/bin/install -m 755 ${objdir}/src/phast/win32_2005/merge/phast.exe \
-    ${instdir}${prefix}/bin/phast-mpich2.exe && \
-  /usr/bin/install -m 755 ${objdir}/src/phast/win32_2005/ser/phast.exe \
-    ${instdir}${prefix}/bin/phast-ser.exe && \
-  /usr/bin/install -m 755 ${objdir}/Redist/hdf5dll.dll \
-    ${instdir}${prefix}/bin/hdf5dll.dll && \
-  /usr/bin/install -m 755 ${objdir}/Redist/zlib1.dll \
-    ${instdir}${prefix}/bin/zlib1.dll && \
-  /usr/bin/install -m 755 ${objdir}/Redist/szlibdll.dll \
-    ${instdir}${prefix}/bin/szlibdll.dll && \
-  /usr/bin/install -m 755 ${objdir}/bin/${PKG}.bat \
-    ${instdir}${prefix}/bin/${PKG}.bat && \
-# doc directory
-  /usr/bin/install -m 644 ${objdir}/doc/phast.pdf \
-    ${instdir}${prefix}/doc/phast.pdf && \
-  /usr/bin/install -m 644 ${objdir}/doc/phreeqc.pdf \
-    ${instdir}${prefix}/doc/phreeqc.pdf && \
-  /usr/bin/install -m 644 ${objdir}/doc/wrir02-4172.pdf \
-    ${instdir}${prefix}/doc/wrir02-4172.pdf && \
-  /usr/bin/install -m 644 ${srcdir}/src/phast/phreeqc.revisions \
-    ${instdir}${prefix}/doc/phreeqc.revisions.txt && \
-  /usr/bin/unix2dos ${instdir}${prefix}/doc/phreeqc.revisions.txt && \
-  /usr/bin/install -m 644 ${srcdir}/src/phast/revisions \
-    ${instdir}${prefix}/doc/RELEASE.txt && \
-  /usr/bin/unix2dos ${instdir}${prefix}/doc/RELEASE.txt && \
-# src directories
-  /usr/bin/cp -r ${srcdir}/src ${instdir}${prefix}/. && \
-  /usr/bin/rm -rf ${instdir}${prefix}/src/phasthdf/hdf-java &&\
-  /usr/bin/rm -rf ${instdir}${prefix}/src/phasthdf/test &&\
-  /usr/bin/find ${instdir}${prefix}/src/phasthdf/. -type f -name ".nbattrs" -exec /usr/bin/rm -f {} \; && \
-# src/phast directory
-  /usr/bin/rm -f ${instdir}${prefix}/src/phast/phreeqc.revisions &&\
-  /usr/bin/rm -rf ${instdir}${prefix}/src/phast/phreeqc/Sun &&\
-# src/phasthdf directory
-  /usr/bin/find ${instdir}${prefix}/src/phastinput/. -name "*.java" -exec /usr/bin/unix2dos {} \; && \
-# database directory
-  /usr/bin/install -m 644 ${objdir}/database/*.dat \
-    ${instdir}${prefix}/database/. && \
-  /usr/bin/unix2dos ${instdir}${prefix}/database/*.dat && \
-# clean up .cvsignore files
-  /usr/bin/find ${instdir}${prefix}/src -name .cvsignore -exec /usr/bin/rm -f {} \; && \
-# examples directory
-  for arg in $EXAMPLES; do
-    /usr/bin/cp -r ${objdir}/examples/${arg}/* ${instdir}${prefix}/examples/${arg}/.
-  done && \
-  /usr/bin/find ${instdir}${prefix}/examples/. -type f -exec /usr/bin/unix2dos {} \; && \
-  /usr/bin/find ${instdir}${prefix}/examples/. -type f -exec /usr/bin/chmod 644 {} \; && \
-# phastexport
-  /usr/bin/install -m 755 ${objdir}/src/phasthdf/win32/Release/phasthdf.exe \
-    ${instdir}${prefix}/export/bin/phasthdf.exe && \
-  /usr/bin/install -m 644 ${objdir}/src/phasthdf/dist/*.jar \
-    ${instdir}${prefix}/export/lib && \
-  /usr/bin/install -m 755 ${objdir}/src/phasthdf/dist/Win32/*.dll \
-    ${instdir}${prefix}/export/lib/Win32 && \
-# ModelViewer root directory
-  /usr/bin/install -m 644 ${objdir}/Redist/notice.txt \
-    ${instdir}${prefix}/mv/. && \
-  /usr/bin/install -m 644 ${objdir}/Redist/readme.txt \
-    ${instdir}${prefix}/mv/. && \
-# ModelViewer doc directory
-  /usr/bin/install -m 644 ${objdir}/Redist/ofr02-106.pdf \
-    ${instdir}${prefix}/mv/doc/. && \
-# ModelViewer bin directory
-  /usr/bin/install -m 644 ${objdir}/Redist/modview.chm \
-    ${instdir}${prefix}/mv/bin/. && \
-  /usr/bin/install -m 644 ${objdir}/Redist/DFORRT.DLL \
-    ${instdir}${prefix}/mv/bin/. && \
-  /usr/bin/install -m 755 ${objdir}/Redist/hdf5dll.dll \
-    ${instdir}${prefix}/mv/bin/. && \
-  /usr/bin/install -m 644 ${objdir}/Redist/lf90.eer \
-    ${instdir}${prefix}/mv/bin/. && \
-  /usr/bin/install -m 755 ${objdir}/Redist/lf90wiod.dll \
-    ${instdir}${prefix}/mv/bin/. && \
-  /usr/bin/install -m 755 ${objdir}/Mv/ModelViewer/Release/*.dll \
-    ${instdir}${prefix}/mv/bin/. && \
-  /usr/bin/install -m 755 ${objdir}/Mv/Modflow2000Reader1/mf2k_r1.dll \
-    ${instdir}${prefix}/mv/bin/. && \
-  /usr/bin/install -m 755 ${objdir}/Mv/Modflow96Reader1/mf96_r1.dll \
-    ${instdir}${prefix}/mv/bin/. && \
-  /usr/bin/install -m 755 ${objdir}/Mv/Mt3dmsReader1/mt_r1.dll \
-    ${instdir}${prefix}/mv/bin/. && \
-  /usr/bin/install -m 755 ${objdir}/Mv/ModelViewer/Release/modview.exe \
-    ${instdir}${prefix}/mv/bin/. && \
-  /usr/bin/install -m 755 ${objdir}/Redist/vtkCommon.dll \
-    ${instdir}${prefix}/mv/bin/. && \
-  /usr/bin/install -m 755 ${objdir}/Redist/vtkFiltering.dll \
-    ${instdir}${prefix}/mv/bin/. && \
-  /usr/bin/install -m 755 ${objdir}/Redist/vtkGraphics.dll \
-    ${instdir}${prefix}/mv/bin/. && \
-  /usr/bin/install -m 755 ${objdir}/Redist/vtkImaging.dll \
-    ${instdir}${prefix}/mv/bin/. && \
-  /usr/bin/install -m 755 ${objdir}/Redist/vtkRendering.dll \
-    ${instdir}${prefix}/mv/bin/. && \
-  /usr/bin/install -m 755 ${objdir}/Redist/zlib1.dll \
-    ${instdir}${prefix}/mv/bin/. && \
-  /usr/bin/install -m 755 ${objdir}/Redist/szlibdll.dll \
-    ${instdir}${prefix}/mv/bin/. && \
-# InstallShield compile
-  "${IS_COMPILER}" "${IS_RULFILES}" -I"${IS_INCLUDEIFX}" -I"${IS_INCLUDEISRT}" \
-    -I"${IS_INCLUDESCRIPT}" "${IS_LINKPATH1}" "${IS_LINKPATH2}" ${IS_LIBRARIES} \
-    ${IS_DEFINITIONS} ${IS_SWITCHES} && \
-# InstallShield build
-  "${IS_BUILDER}" -p"${IS_INSTALLPROJECT}" -m"${IS_CURRENTBUILD}" && \
+  (rm -rf ${instdir}/* && \
 # logs
-  /usr/bin/install -m 755 ${objdir}/src/phasthdf/win32/phastexport.plg \
-    ${instdir}/logs/. && \
+  /usr/bin/install -m 755 ${objdir}/phasthdf.log \
+    ${instdir}/. && \
   /usr/bin/install -m 644 ${objdir}/phastinput.log \
-    ${instdir}/logs/. && \
+    ${instdir}/. && \
   /usr/bin/install -m 644 ${objdir}/phast-ser.log \
-    ${instdir}/logs/. && \
+    ${instdir}/. && \
   /usr/bin/install -m 644 ${objdir}/phast-merge.log \
-    ${instdir}/logs/. && \
-  /usr/bin/install -m 644 ${objdir}/Mv/mv/mv.plg \
-    ${instdir}/logs/. && \
-  /usr/bin/install -m 644 "${objdir}/packages/win32-is/Media/SingleDisk/Log Files/"* \
-  ${instdir}/logs/. && \
-  /usr/bin/install -m 644 "${objdir}/packages/win32-is/Media/SingleDisk/Report Files/"* \
-  ${instdir}/logs/. && \
-# the installation executable
-  /usr/bin/install -m 755 "${objdir}/packages/win32-is/Media/SingleDisk/Disk Images/Disk1/setup.exe" \
-    ${instdir}/${FULLPKG}.exe &&\
-  run_examples )
+    ${instdir}/. && \
+  /usr/bin/install -m 644 ${objdir}/ModelViewer/mv/mv.plg \
+    ${instdir}/. && \
+# the MSI
+  /usr/bin/install -m 755 "${objdir}/msi/bin/Release/${FULLPKG}.msi" \
+    ${instdir}/. && \
+# md5sums    
+  if [ -x /usr/bin/md5sum ]; then \
+    cd ${instdir} && \
+    find . -type f ! -name md5sum | sed 's/^/\"/' | sed 's/$/\"/' | xargs md5sum > md5sum ; \
+  fi )    
 }
 
 run_examples() {
@@ -515,7 +323,6 @@ run_examples() {
   done )
 }
 
-
 strip() {
   (cd ${instdir} && \
   echo No need to strip )
@@ -530,7 +337,6 @@ mkpatch() {
   (cd ${srcdir} && \
   tar xvzf ${src_orig_pkg} && \
   cd ${srcdir}/${BASEPKG} && \
-  tar xvzf ${src_orig_pkg_mv} && \
   cd ${srcdir} && \
   mv ${BASEPKG} ../${BASEPKG}-orig && \
   cd ${topdir} && \
@@ -538,11 +344,12 @@ mkpatch() {
     ${DIFF_IGNORE} \
     ${BASEPKG}-orig ${BASEPKG} > \
     ${srcinstdir}/${src_patch_name} ; \
-  rm -rf ${BASEPKG}-orig )}
+  rm -rf ${BASEPKG}-orig )
+}
+
 spkg() {
   (mkpatch && \
   cp -al ${src_orig_pkg} ${srcinstdir}/${src_orig_pkg_name} && \
-  cp -al ${src_orig_pkg_mv} ${srcinstdir}/. && \
   cp -al $0 ${srcinstdir}/`basename $0` && \
   cp Makefile ${srcinstdir}/. && \
   cd ${srcinstdir} && \
