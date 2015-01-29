d06c3c045b6072377728fb0350e61d78443be04a
Scott R Charlton <charlton@usgs.gov>
2015-01-29 01:49:05 +0000

    fixing bad copy from: internalbrr.cr.usgs.gov/svn_GW/phastpp/branches/phastpp3-F90
    git-svn-id: svn://136.177.114.72/svn_GW/phast3/trunk@9221 1feff8c3-07ed-0310-ac33-dd36852eb9cd

--- phastpp3-F90/src/phast/PhastFortran/clog.f90
+++
@@ -1,7 +0,0 @@
-SUBROUTINE clog(string)  
-  ! ... Writes a string to the CLOG file
-  USE f_units, ONLY: fuclog
-  CHARACTER(LEN=*) :: string  
-  !     ------------------------------------------------------------------
-  WRITE(fuclog,'(A)') string  
-END SUBROUTINE clog
